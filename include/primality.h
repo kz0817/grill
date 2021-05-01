daaade7515c12a68b0a4bb339c8afa61da684f4d
Kazuhiro Yamato <kz0817@gmail.com>
2021-05-02 08:11:34 +0900

    primality: remove unncessary lines.

--- include/primality.h
+++ include/primality.h
@@ -3,12 +3,5 @@
 #include "integer.h"
 
 class primality {
-    primality();
-    virtual ~primality();
-
     static bool is_prime(const integer& n);
-
-private:
-    struct context;
-    std::unique_ptr<context> ctx;
 };
