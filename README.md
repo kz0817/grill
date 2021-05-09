9234ada420307a08104df74c6e744cef6832a9a8
Kazuhiro Yamato <kz0817@gmail.com>
2021-05-09 11:04:20 +0900

    use Leaf class library for parsing arguments.

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # grill
-grill is a library including a wide digit integer and mathematical functions using it.
+grill is a library including a wide digit integer class and mathematical functions
+using it.
 
 # How to build
+
+## Install depending library
+
+    cd `<grill's top directory>`
+    git clone git@github.com:kz0817/Leaf.git
+
+or you can pull only the latest revision with `--depth 1`.
+
+    git clone --detph 1 git@github.com:kz0817/Leaf.git
+
+## Build
 
     ./autogen.sh
     ./configure
--- tools/create-integers.cc
+++ tools/create-integers.cc
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include "integer.h"
+#include "ArgParser.h"
 
 using namespace grill;
+using namespace Leaf;
 
-static const int NumMandatoryArgs = 1;
-static const char Usage[] = R"(
-usage:
-    create_instances NUM_CREATE
-)";
-
-static void show_usage() {
-    std::cout << Usage << std::endl;
-}
+struct options_def {
+    bool show_help = false;
+    std::size_t num = 1;
+};
 
 static void run(const std::size_t num) {
     for (std::size_t i = 0; i < num; i++) {
@@ -21,14 +18,34 @@ static void run(const std::size_t num) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < NumMandatoryArgs + 1) {
-        show_usage();
+    ArgParser<options_def> parser("create-integers", "creates integer objects many times",
+                                  "create-integers -n N");
+    parser.add({"-h", "--help"}, [](options_def& opt, ...) {
+        opt.show_help = true;
+    }, "", "Show this help message.");
+    parser.add({"-n"}, [](options_def& opt, ArgParser<options_def>& parser) {
+        if (!parser.hasNext()) {
+            parser.error("-n: parameter is required");
+            return;
+        }
+        opt.num = std::stol(parser.getNext());
+    }, "N", "Number of integer objects to be created" );
+
+    if (!parser.parse(argc, argv)) {
+        std::cout << parser.getErrorMessage() << std::endl;
+        std::cout << std::endl;
+        std::cout << parser.generateUsage() << std::endl;
         return EXIT_FAILURE;
     }
 
-    const std::size_t num = std::stol(argv[1]);
-    std::cout << "Number of creation: " << num << std::endl;
-    run(num);
+    const options_def& options = parser.getPrivateData();
+    if (options.show_help) {
+        std::cout << parser.generateUsage() << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    std::cout << "Number of creation: " << options.num << std::endl;
+    run(options.num);
 
     return EXIT_SUCCESS;
 }
