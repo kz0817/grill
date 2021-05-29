b4b36e25a88ba8c29a2dbeac282e1849a1ef8239
Kazuhiro Yamato <kz0817@gmail.com>
2021-05-29 09:09:39 +0900

    prime-number: support fermat test and miller-rabin test.

--- tools/prime-number.cc
+++ tools/prime-number.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <unordered_map>
 #include "ArgParser.h"
 #include "integer.h"
 #include "primality.h"
@@ -8,19 +9,69 @@
 using namespace grill;
 using namespace Leaf;
 
+enum class algorithm {
+    trivial_division,
+    fermat_test,
+    miller_rabin_test,
+    unknown,
+};
+
+static const std::unordered_map<algorithm, std::string> algorithm_name_map = {
+    {algorithm::trivial_division, "trivial_division"},
+    {algorithm::fermat_test, "fermat_test"},
+    {algorithm::miller_rabin_test, "miller_rabin_test"},
+};
+
+std::string to_string(const algorithm name) {
+    const auto it = algorithm_name_map.find(name);
+    assert(it != algorithm_name_map.end());
+    return it->second;
+}
+
 struct options_def {
     bool show_help = false;
     bool primitive = false;
+    algorithm test_algorithm = algorithm::trivial_division;
     std::size_t num = 2;
 };
 
-static void run(const options_def& options) {
-    std::cout << "Number: " << options.num << std::endl;
-    std::cout << "Type type: " << (options.primitive ? "primitive" : "integer") << std::endl;
+static const std::unordered_map<std::string, algorithm> algorithm_map = {
+    {"trivial_division", algorithm::trivial_division},
+    {"fermat_test", algorithm::fermat_test},
+    {"miller_rabin_test", algorithm::miller_rabin_test},
+};
 
-    const bool is_prime = options.primitive ? primality::trivial_division(options.num)
-                                            : primality::trivial_division(wide_int<64>(options.num));
-    std::cout << "is_prime: " << util::to_string(is_prime) << std::endl;
+static algorithm parse_algorithm(const std::string& name) {
+    const auto it = algorithm_map.find(name);
+    if (it == algorithm_map.end())
+        return algorithm::unknown;
+    return it->second;
+}
+
+static const std::unordered_map<algorithm, std::function<bool(const options_def&)>> proc_map = {
+    {algorithm::trivial_division, [](const options_def& options) {
+        std::cout << "Type type: " << (options.primitive ? "primitive" : "integer") << std::endl;
+        return options.primitive ? primality::trivial_division(options.num)
+                                 : primality::trivial_division(wide_int<64>(options.num));
+    }},
+    {algorithm::fermat_test, [](const options_def& options) {
+        return primality::fermat_test(wide_int<64>(options.num));
+    }},
+    {algorithm::miller_rabin_test, [](const options_def& options) {
+        return primality::miller_rabin_test(wide_int<64>(options.num));
+    }},
+};
+
+static void run(const options_def& options) {
+    std::cout << "Number   : " << options.num << std::endl;
+    std::cout << "Algorithm: " << to_string(options.test_algorithm) << std::endl;
+
+    const auto proc_it = proc_map.find(options.test_algorithm);
+    assert(proc_it != proc_map.end());
+    const auto proc = proc_it->second;
+
+    const bool is_prime = proc(options);
+    std::cout << "is_prime : " << util::to_string(is_prime) << std::endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -33,6 +84,20 @@ int main(int argc, char *argv[]) {
     parser.add({"-p", "--primitive"}, [](options_def& opt, ...) {
         opt.primitive = true;
     }, "", "Use the primitive number type instead of the integer class.");
+
+    parser.add({"-a", "--algorithm"}, [](options_def& opt, ArgParser<options_def>& parser) {
+        if (!parser.hasNext()) {
+            parser.error("-a: parameter is required");
+            return;
+        }
+
+        const std::string algorithm_name = parser.getNext();
+        opt.test_algorithm = parse_algorithm(algorithm_name);
+        if (opt.test_algorithm == algorithm::unknown) {
+            parser.error("Unknwon algorithm: " + algorithm_name);
+            return;
+        }
+    }, "A", "Algorithm (trivial_division, fermat_test, or miller_rabin_test)");
 
     parser.add({"-n"}, [](options_def& opt, ArgParser<options_def>& parser) {
         if (!parser.hasNext()) {
