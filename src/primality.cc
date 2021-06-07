2732c7e87236c90794a9dad07613ee8068d7d88e
Kazuhiro Yamato <kz0817@gmail.com>
2021-06-07 21:35:42 +0900

    primality: remove the unnecessary condition.

--- src/primality.cc
+++ src/primality.cc
@@ -58,8 +58,6 @@ bool primality::fermat_test(const integer& n) {
     for (const auto& a: fermat_test_data) {
         if (a >= n)
             continue;
-        if (util::gcd(a, n) != constant::One)
-             return false;
         if (!pass_fermat_little_theorem(n, a))
             return false;
     }
