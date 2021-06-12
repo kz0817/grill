cdcb1ada0670a405eb881d3e8b9e0e87c5028d14
Kazuhiro Yamato <kz0817@gmail.com>
2021-06-12 22:02:42 +0900

    block_allocator: be simple.
    The performance is degraded. But I'll imporve the next commit.
    
    << this revision >>
    $ time tools/create-integers -n $((10**8))
    Number of creation: 100000000
    
    real    0m4.279s
    user    0m4.274s
    sys     0m0.010s
    
    << previous revision >>
    $  time tools/create-integers -n $((10**8))
    Number of creation: 100000000
    
    real    0m4.017s
    user    0m4.009s
    sys     0m0.018s

--- src/block_allocator.cc
+++ src/block_allocator.cc
@@ -6,40 +6,34 @@
 
 namespace grill {
 
-struct block_packet_list_head;
-
 struct block_packet {
-    block_packet_list_head* list_head;
     block_packet *next;
+    std::size_t num_blocks;
     integer::block_t blocks[];
 };
 static_assert(std::is_standard_layout<block_packet>::value, "block_packet must be standard layout");
 
-struct block_packet_list_head {
-    block_packet *next = nullptr;
-};
-
 static constexpr std::size_t OffsetBlocks = offsetof(block_packet, blocks);
 
 struct block_allocator::private_context {
-    std::map<int, block_packet_list_head *> cache_table;
+    std::map<int, block_packet*> cache_table;
 
-    block_packet_list_head* get_list_head(const std::size_t num_blocks) {
+    block_packet* get_list_head(const std::size_t num_blocks) {
         const auto it = this->cache_table.find(num_blocks);
         if (it != this->cache_table.end())
             return it->second;
-
-        block_packet_list_head* head = new block_packet_list_head;
-        this->cache_table[num_blocks] = head;
-        return head;
+        return nullptr;
     }
 
-    block_packet* create_block_packet(const std::size_t num_blocks,
-                                      block_packet_list_head *list_head) {
+    void set_list_head(const std::size_t num_blocks, block_packet* pkt) {
+        this->cache_table[num_blocks] = pkt;
+    }
+
+    block_packet* create_block_packet(const std::size_t num_blocks) {
         const std::size_t pkt_size = sizeof(block_packet) + sizeof(integer::block_t) * num_blocks;
         block_packet* pkt = reinterpret_cast<block_packet *>(new uint8_t[pkt_size]);
-        pkt->list_head = list_head;
         pkt->next = nullptr;
+        pkt->num_blocks = num_blocks;
         return pkt;
     }
 
@@ -49,13 +43,12 @@ struct block_allocator::private_context {
     }
 
     integer::block_t* take(const std::size_t num_blocks) {
-        block_packet_list_head* head = get_list_head(num_blocks);
-        block_packet *pkt = head->next;
-        if (pkt != nullptr)
-            head->next = pkt->next; // take the first entry out
-        else
-            pkt = create_block_packet(num_blocks, head);
-        return pkt->blocks;
+        block_packet* head = get_list_head(num_blocks);
+        if (head == nullptr)
+            return create_block_packet(num_blocks)->blocks;
+
+        set_list_head(num_blocks, head->next);
+        return head->blocks;
     }
 
     void free(integer::block_t* blocks) {
@@ -63,9 +56,9 @@ struct block_allocator::private_context {
 
         // TODO: add pkt deletion when the number of caches is over the upper limit.
 
-        block_packet_list_head* head = pkt->list_head;
-        pkt->next = head->next;
-        head->next = pkt;
+        block_packet* head = get_list_head(pkt->num_blocks);
+        pkt->next = head;
+        set_list_head(pkt->num_blocks, pkt);
     }
 };
 
