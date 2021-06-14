d175e0a352632cafa7fe12f4a6a4f27d5070397f
Kazuhiro Yamato <kz0817@gmail.com>
2021-06-15 07:18:15 +0900

    primality: add test samples for primality tests.

--- test/test_primality.cc
+++ test/test_primality.cc
@@ -10,6 +10,7 @@ BOOST_AUTO_TEST_SUITE(test_suite_primality)
 static struct sample_t {
     const integer& num;
     bool is_prime_number;
+    bool skip_trivial_div = false;
 
     friend std::ostream& operator<<(std::ostream& os, const sample_t& s) {
         os << "num: " << s.num << ", is_prime_number: " << util::to_string(s.is_prime_number);
@@ -44,16 +45,25 @@ static struct sample_t {
     {wide_int<64>(27), false},
     {wide_int<64>(28), false},
     {wide_int<64>(29), true},
+    {wide_int<64>(127), true},              // M7(2^7-1)
+    {wide_int<64>(8191), true, true},       // M13 (2^13-1)
+    {wide_int<64>(524'287), true, true},     // M19 (2^19-1)
+    {wide_int<64>(670'0417), true, true},    // (2^32+1)/641
+    {wide_int<64>(2'147'483'647), true, true}, // M31 (2^31-1)
 };
 
 BOOST_DATA_TEST_CASE(test_trivial_division_for_native, samples)
 {
+    if (sample.skip_trivial_div)
+        return;
     const integer::block_t n = sample.num.ref_blocks()[0];
     BOOST_TEST(primality::trivial_division(n) == sample.is_prime_number);
 }
 
 BOOST_DATA_TEST_CASE(test_trivial_division, samples)
 {
+    if (sample.skip_trivial_div)
+        return;
     BOOST_TEST(primality::trivial_division(sample.num) == sample.is_prime_number);
 }
 
