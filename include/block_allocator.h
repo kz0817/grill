a28733432ae55d834f10fb3c8b975e6bbf34eb5b
Kazuhiro Yamato <kz0817@gmail.com>
2021-06-16 06:17:22 +0900

    make block_allocator a template class to improve performance.
    [create-integer]
    << this revision >>
    $ time tools/create-integers -n $((10**8))
    Number of creation: 100000000
    
    real    0m3.453s
    user    0m3.442s
    sys     0m0.019s
    
    << previsou revision >>
    $ time tools/create-integers -n $((10**8))
    Number of creation: 100000000
    
    real    0m3.959s
    user    0m3.949s
    sys     0m0.019s
    
    [prime-number]
    << this revision>>
    $ time tools/prime-number -a trivial_division -n $((2**31-1))
    Number   : 2147483647
    Algorithm: trivial_division
    Type type: integer
    is_prime : true
    
    real    0m3.099s
    user    0m3.098s
    sys     0m0.009s
    
    << previous revision >>
    $ time tools/prime-number -a trivial_division -n $((2**31-1))
    Number   : 2147483647
    Algorithm: trivial_division
    Type type: integer
    is_prime : true
    
    real    0m3.587s
    user    0m3.584s
    sys     0m0.007s
    
    [perf's profiling result of this revision for prime-number]
    $ sudo perf_4.19 record tools/prime-number -a trivial_division -n $((2**31-1))
    Number   : 2147483647
    Algorithm: trivial_division
    Type type: integer
    is_prime : true
    [ perf record: Woken up 2 times to write data ]
    [ perf record: Captured and wrote 0.483 MB perf.data (12142 samples) ]
    
    $  sudo perf_4.19 report | head -30
        16.50%  prime-number  libgrill.so.0.0.0  [.] grill::integer::~integer
        13.71%  prime-number  libgrill.so.0.0.0  [.] grill::integer::integer
         8.32%  prime-number  libgrill.so.0.0.0  [.] grill::integer::most_significant_active_bit
         8.01%  prime-number  libgrill.so.0.0.0  [.] grill::integer::operator*
         5.01%  prime-number  libgrill.so.0.0.0  [.] grill::add
         3.99%  prime-number  libgrill.so.0.0.0  [.] grill::div
         3.80%  prime-number  libgrill.so.0.0.0  [.] grill::compare
         3.77%  prime-number  libc-2.28.so       [.] __memset_avx2_unaligned_erms
         3.62%  prime-number  ld-2.28.so         [.] __tls_get_addr
         3.47%  prime-number  libgrill.so.0.0.0  [.] grill::integer::pow2
         3.16%  prime-number  libgrill.so.0.0.0  [.] grill::integer::get_num_blocks
         2.57%  prime-number  libgrill.so.0.0.0  [.] grill::integer::get_num_blocks@plt
         2.24%  prime-number  libgrill.so.0.0.0  [.] grill::integer::integer
         2.17%  prime-number  libgrill.so.0.0.0  [.] grill::integer::get_blocks@plt
         2.11%  prime-number  libgrill.so.0.0.0  [.] grill::integer::get_blocks
         1.81%  prime-number  libgrill.so.0.0.0  [.] grill::integer::set_bit_value
         1.44%  prime-number  libgrill.so.0.0.0  [.] grill::integer::ref_blocks@plt
         1.44%  prime-number  libgrill.so.0.0.0  [.] __tls_get_addr@plt
         1.39%  prime-number  libgrill.so.0.0.0  [.] grill::integer::ref_blocks

--- include/block_allocator.h
+++ include/block_allocator.h
@@ -1,20 +1,89 @@
 #pragma once
 #include <memory>
+#include <type_traits>
+#include <cstddef>
+#include <cassert>
 #include "integer.h"
 
 namespace grill {
 
 class block_allocator {
 public:
-    block_allocator();
-    virtual ~block_allocator();
+    block_allocator() {
+        this->cache_tables = new block_packet*[this->cache_table_size];
+        for (std::size_t i = 0; i < this->cache_table_size; i++)
+            this->cache_tables[i] = nullptr;
+    }
 
-    integer::block_t* take(const std::size_t num_blocks);
-    void free(integer::block_t* blocks);
+    virtual ~block_allocator() {
+        // TODO: free cache_table
+    }
+
+    integer::block_t* take(const std::size_t num_blocks) {
+        block_packet* head = get_list_head(num_blocks);
+        if (head == nullptr)
+            return create_block_packet(num_blocks)->blocks;
+
+        set_list_head(num_blocks, head->next);
+        return head->blocks;
+    }
+
+    void free(integer::block_t* blocks) {
+        block_packet* pkt = calc_object_addr(blocks);
+
+        // TODO: add pkt deletion when the number of caches is over the upper limit.
+
+        block_packet* head = get_list_head(pkt->num_blocks);
+        pkt->next = head;
+        set_list_head(pkt->num_blocks, pkt);
+    }
 
 private:
-    struct private_context;
-    std::unique_ptr<private_context> ctx;
+    struct block_packet {
+        block_packet *next;
+        std::size_t num_blocks;
+        integer::block_t blocks[];
+    };
+    static_assert(std::is_standard_layout<block_packet>::value,
+                  "block_packet must be standard layout");
+
+    static constexpr std::size_t DEFAULT_TABLE_SIZE = 256; // 16384 (8192*2) bit
+    static constexpr std::size_t OffsetBlocks = offsetof(block_packet, blocks);
+    block_packet** cache_tables = nullptr;
+    std::size_t cache_table_size = DEFAULT_TABLE_SIZE;
+
+    block_packet* get_list_head(const std::size_t num_blocks) {
+        const std::size_t idx = num_blocks - 1;
+        if (idx >= this->cache_table_size)
+            return nullptr;
+        return this->cache_tables[idx];
+    }
+
+    void set_list_head(const std::size_t num_blocks, block_packet* pkt) {
+        const std::size_t idx = num_blocks - 1;
+        if (idx >= this->cache_table_size)
+            resize_cache_table(num_blocks);
+
+        this->cache_tables[idx] = pkt;
+    }
+
+    void resize_cache_table(const std::size_t num_tables) {
+        // TODO: implement
+        assert(false);
+    }
+
+    block_packet* create_block_packet(const std::size_t num_blocks) {
+        const std::size_t pkt_size = sizeof(block_packet) + sizeof(integer::block_t) * num_blocks;
+        block_packet* pkt = reinterpret_cast<block_packet *>(new uint8_t[pkt_size]);
+        pkt->next = nullptr;
+        pkt->num_blocks = num_blocks;
+        return pkt;
+    }
+
+    block_packet* calc_object_addr(integer::block_t* blocks) {
+        uint8_t *addr = reinterpret_cast<uint8_t*>(blocks);
+        return reinterpret_cast<block_packet *>(addr - OffsetBlocks);
+    }
 };
 
 } // namespace grill
