3b594175cd100b680fd4f24e1a4e82ecc38349c3
Kazuhiro Yamato <kz0817@gmail.com>
2021-06-19 10:54:04 +0900

    util: Uses namespace instead of class according to the coding convention.

--- include/util.h
+++ include/util.h
@@ -2,30 +2,36 @@
 #include "Integer.h"
 
 namespace grill {
+namespace util {
 
-class util {
-public:
-    static Integer gcd(const Integer& a, const Integer& b);
-    static std::string to_string(const bool b);
+Integer gcd(const Integer& a, const Integer& b);
 
-    /**
-     * Convert an Integer instance to an unsigned long.
-     *
-     * The part over the range of an unsigned int is ignored.
-     *
-     * @param n An Integer.
-     * @return A converted unsigned int.
-     */
-    static unsigned long to_uint(const Integer& n);
+/**
+* Convert a bool value to "true" or "false".
+*
+* @param b A bool value.
+* @return "true" or "false".
+*/
+std::string to_string(const bool b);
 
-    /**
-     * Convert a number string to Integer.
-     *
-     * If a number string begins from 0x, it is handled as a hex decimal number.
-     *
-     * @param s A number string.
-     */
-    static Integer to_Integer(const std::string& s);
-};
+/**
+* Convert an Integer instance to an unsigned long.
+*
+* The part over the range of an unsigned int is ignored.
+*
+* @param n An Integer.
+* @return A converted unsigned int.
+*/
+unsigned long to_uint(const Integer& n);
 
+/**
+* Convert a number string to Integer.
+*
+* If a number string begins from 0x, it is handled as a hex decimal number.
+*
+* @param s A number string.
+*/
+Integer to_Integer(const std::string& s);
+
+} // namespace util
 } // namespace grill
