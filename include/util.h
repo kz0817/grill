cdd665ca0ba4435e0ed1211dee67f22bc10f8fca
Kazuhiro Yamato <kz0817@gmail.com>
2021-06-24 07:40:32 +0900

    util: Fix indents and typo

--- include/util.h
+++ include/util.h
@@ -7,30 +7,30 @@ namespace util {
 Integer gcd(const Integer& a, const Integer& b);
 
 /**
-* Convert a bool value to "true" or "false".
-*
-* @param b A bool value.
-* @return "true" or "false".
-*/
+ * Convert a bool value to "true" or "false".
+ *
+ * @param b A bool value.
+ * @return "true" or "false".
+ */
 std::string to_string(const bool b);
 
 /**
-* Convert an Integer instance to an unsigned long.
-*
-* The part over the range of an unsigned int is ignored.
-*
-* @param n An Integer.
-* @return A converted unsigned int.
-*/
+ * Convert an Integer instance to an unsigned long.
+ *
+ * The part over the range of an unsigned int is ignored.
+ *
+ * @param n An Integer.
+ * @return A converted unsigned int.
+ */
 unsigned long to_uint(const Integer& n);
 
 /**
-* Convert a number string to Integer.
-*
-* If a number string begins from 0x, it is handled as a hex decimal number.
-*
-* @param s A number string.
-*/
+ * Convert a number string to Integer.
+ *
+ * If a number string begins from 0x, it is handled as a hexadecimal number.
+ *
+ * @param s A number string.
+ */
 Integer to_Integer(const std::string& s);
 
 } // namespace util
