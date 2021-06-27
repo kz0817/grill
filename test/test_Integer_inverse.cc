214796a990e12500d5f56cb3a114207575342df3
Kazuhiro Yamato <kz0817@gmail.com>
2021-06-28 08:09:00 +0900

    Add test sample for inverse() that causes failure.

--- test/test_Integer_inverse.cc
+++ test/test_Integer_inverse.cc
@@ -19,6 +19,10 @@ static binary_op_sample_t inverse_samples[] = {
     {WideInt<64>(9), WideInt<64>(5), WideInt<64>(4)},
 
     // TODO: Add samples with greater digits.
+    {WideInt<64>(1), WideInt<64>(0x1a770ecab58cfb0d), WideInt<64>(1)},
+    {WideInt<64>(3), WideInt<64>(0x1a770ecab58cfb0d), WideInt<64>(0x11a4b48723b35209)},
+    {WideInt<64>(3), WideInt<64>(0x1a770eca0ecaba9c), WideInt<64>(0x11a4b486b4872713)},
+    {WideInt<64>(0x10001), WideInt<64>(0x1a770eca0ecaba9c), WideInt<64>(0x88c80f88042b879)},
 };
 
 BOOST_DATA_TEST_CASE(inverse, inverse_samples)
