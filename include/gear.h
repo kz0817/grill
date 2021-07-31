36368f16c092774c42513dbe1096b18f8838301d
Kazuhiro Yamato <kz0817@gmail.com>
2021-07-31 12:54:52 +0900

    gear: Add mul().

--- include/gear.h
+++ include/gear.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 
 namespace grill {
 namespace gear {
@@ -23,6 +24,15 @@ void fill_zero(T* dest, std::size_t n) {
     for (std::size_t i = 0; i < n; i++)
         dest[i] = 0;
 }
+
+/**
+ * Multiply two numbers.
+ *
+ * @param out the output buffer.
+ * @param in0 A number.
+ * @param in1 An another number.
+ */
+void mul(uint64_t out[2], const uint64_t in0, const uint64_t in1);
 
 } // namespace gear
 } // namespace grill
