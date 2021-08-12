b93d6ec1870b10642642fc618264b95c3b36c377
Kazuhiro Yamato <kz0817@gmail.com>
2021-08-12 14:01:53 +0900

    gear: add add() and sub().

--- include/gear.h
+++ include/gear.h
@@ -25,6 +25,59 @@ void fill_zero(T* dest, std::size_t n) {
         dest[i] = 0;
 }
 
+template<typename T>
+struct AddOp {
+    static bool calc(T& lhs, const T& rhs, const T prev_lhs) {
+        lhs += rhs;
+        return lhs < prev_lhs;
+    }
+
+    static bool handle_carry(T& lhs) {
+        lhs++;
+        return lhs == 0;
+    }
+};
+
+template<typename T>
+struct SubOp {
+    static bool calc(T& lhs, const T& rhs, const T prev_lhs) {
+        lhs -= rhs;
+        return lhs > prev_lhs;
+    }
+
+    static bool handle_carry(T& lhs) {
+        lhs--;
+        return lhs == static_cast<T>(-1);
+    }
+};
+
+template <typename T, typename OP>
+bool do_add_sub(T* dest, const std::size_t num_dest, const T* in, const std::size_t num_in) {
+    bool carry_flag = false;
+    for (std::size_t i = 0; i < num_dest; i++) {
+        const bool in_is_valid = (i < num_in);
+        if (!in_is_valid && !carry_flag)
+            break;
+
+        if (carry_flag)
+            carry_flag = OP::handle_carry(dest[i]);
+
+        const T in_val = in_is_valid ? in[i] : 0;
+        carry_flag |= OP::calc(dest[i], in_val, dest[i]);
+    }
+    return carry_flag;
+}
+
+template <typename T>
+bool add(T* dest, const std::size_t num_dest, const T* in, const std::size_t num_in) {
+    return do_add_sub<T, AddOp<T>>(dest, num_dest, in, num_in);
+}
+
+template <typename T>
+bool sub(T* dest, const std::size_t num_dest, const T* in, const std::size_t num_in) {
+    return do_add_sub<T, SubOp<T>>(dest, num_dest, in, num_in);
+}
+
 void mul(uint64_t out[2], const uint64_t in0, const uint64_t in1);
 
 void karatsuba(uint64_t* out, std::size_t n_out,
--- src/gear.cc
+++ src/gear.cc
@@ -1,3 +1,4 @@
+#include <cassert>
 #include "gear.h"
 
 namespace grill {
@@ -29,17 +30,109 @@ void gear::mul(uint64_t out[2], const uint64_t in0, const uint64_t in1) {
     out[1] = x2;
 
     const uint64_t x1a = in0l * in1u;
-    add(out, (x1a << 32));
+    grill::add(out, (x1a << 32));
     out[1] += upper(x1a);
 
     const uint64_t x1b = in0u * in1l;
-    add(out, (x1b << 32));
+    grill::add(out, (x1b << 32));
     out[1] += upper(x1b);
 }
 
-void gear::karatsuba(uint64_t* dest, std::size_t n_dest,
-                     const uint64_t* n0, std::size_t num_n0,
-                     const uint64_t* n1, std::size_t num_n1) {
+
+struct KaratubaInput {
+    static constexpr uint64_t One = 1;
+    const std::size_t num_lower;
+    const std::size_t num_upper;
+    const uint64_t* lower;
+    const uint64_t* upper;
+
+    KaratubaInput(const std::size_t num_lower_half, const std::size_t num_upper_half,
+                  const uint64_t* in, std::size_t num_in)
+    : num_lower((num_in >= num_lower_half) ? num_lower_half : num_in),
+      num_upper((num_in > num_lower_half) ? num_in - num_lower_half : 0),
+      lower(in),
+      upper((this->num_upper > 0) ? &in[this->num_lower] : nullptr) {
+    }
+
+    /**
+     * @return true if the result is positive. Otherwise false.
+     */
+    bool lower_minus_upper(uint64_t* out, const std::size_t n) const {
+        assert(n == this->num_lower);
+        gear::copy(out, this->lower, n);
+        if (upper == nullptr)
+            return true;
+
+        const bool borrow = gear::sub(out, n, this->upper, this->num_upper);
+        if (borrow)
+            to_positive(out, n);
+        return borrow;
+    }
+
+private:
+    static void to_positive(uint64_t* buf, const std::size_t n) {
+        assert(buf[n-1] & 0x8000'0000'0000'0000);
+        for (std::size_t i = 0; i < n; i++)
+            buf[i] = ~buf[i];
+        gear::add(buf, n, &One, 1);
+    }
+};
+
+static void karatsuba_calc_x1(uint64_t* x1, const std::size_t num_x1,
+                              const uint64_t* x0, const std::size_t num_x0,
+                              const uint64_t* x2, const std::size_t num_x2,
+                              const KaratubaInput& a, const KaratubaInput& b) {
+    gear::fill_zero(x1, num_x1);
+    gear::add(x1, num_x1, x0, num_x0);
+    gear::add(x1, num_x1, x2, num_x2);
+
+    // x1 = x0 + x2 - dx, where dx = da * db
+    // da = a.lower - a.upper
+    // db = b.lower - b.upper
+    uint64_t da[a.num_lower];
+    uint64_t db[b.num_lower];
+    const bool sign_a = a.lower_minus_upper(da, num_x1);
+    const bool sign_b = b.lower_minus_upper(db, num_x1);
+
+    const std::size_t num_dx = a.num_lower + b.num_lower;
+    uint64_t dx[num_dx];
+    gear::karatsuba(dx, num_dx, da, a.num_lower, db, b.num_lower);
+
+    if (sign_a == sign_b)
+        gear::add(x1, num_x1, dx, num_dx);
+    else
+        gear::sub(x1, num_x1, dx, num_dx);
+}
+
+void gear::karatsuba(uint64_t* out, std::size_t n_out,
+                     const uint64_t* in0, std::size_t num_in0,
+                     const uint64_t* in1, std::size_t num_in1) {
+
+    if (num_in0 == 1 && num_in1 == 1) {
+        assert(n_out == 2);
+        mul(out, *in0, *in1);
+        return;
+    }
+
+    // Note: num_lower_half >= num_upper_half
+    const std::size_t max_num = (num_in0 >= num_in1) ? num_in0 : num_in1;
+    const std::size_t num_upper_half = max_num/2;
+    const std::size_t num_lower_half = max_num - num_upper_half;
+
+    const KaratubaInput a(num_lower_half, num_upper_half, in0, num_in0);
+    const KaratubaInput b(num_lower_half, num_upper_half, in1, num_in1);
+
+    // out = x0 + x1*R + x2*R^2, where R=2^64.
+    const std::size_t num_x0 = 2 * num_lower_half;
+    const std::size_t num_x1 = num_lower_half + num_upper_half + 1;
+    const std::size_t num_x2 = 2 * num_upper_half;
+    uint64_t x0[num_x0];
+    uint64_t x1[num_x1];
+    uint64_t x2[num_x2];
+
+    karatsuba(x0, num_x0, a.lower, a.num_lower, b.lower, b.num_lower);
+    karatsuba(x2, num_x1, a.upper, a.num_upper, b.upper, b.num_upper);
+    karatsuba_calc_x1(x1, num_x1, x0, num_x0, x2, num_x2, a, b);
 }
  
 } // namespace grill
